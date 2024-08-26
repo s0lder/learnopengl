5366238942464c8f5965454d79bd172be24739b8
sldr <sldr@laptop>
2024-08-26 21:17:29 +0300

    modified Main.cpp

--- learnopengl/Main.cpp
+++ learnopengl/Main.cpp
@@ -3,8 +3,12 @@
 
 #include <iostream>
 
+void FramebufferResize(GLFWwindow* window, int width, int height);
+void ProcessInput(GLFWwindow* window);
+
 int main()
 {
+	// initializing glfw
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -18,4 +22,79 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, FramebufferResize);
+
+	// initializing GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	const char* vertex_shader_source = "#version 330 core\n"
+		"layout (location = 0) in vec3 a_pos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(a_pos.x, a_pos.y, a_pos.z, 1.0f);\n"
+		"}\0";
+
+	const char* fragment_shader_source = "#version 330 core\n"
+		"out vec4 frag_color;\n"
+		"void main()\n"
+		"{\n"
+		"	frag_color = vec4(1.0f, 0.2f, 0.2f, 1.0f);\n"
+		"}\0";
+
+	float vertices[9] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// create vertex shader
+	unsigned int vertex_shader;
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+
+	// create fragment shader
+	unsigned int fragment_shader;
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+	
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		ProcessInput(window);
+
+		// rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void FramebufferResize(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
